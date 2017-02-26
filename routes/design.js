5a86664e1d492cc63be4c09b9d6f461970eb133d
androyd09 <stevelight16@gmail.com>
2017-02-25 18:00:52 -0700

    UI / pages
    Added more permanent styling to the navigation for the main pages
    added subpage navigation in the router
    added a bunch of colors for templating
    switched the partials for the subpages

--- routes/design.js
+++ routes/design.js
@@ -4,10 +4,47 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('design/index', {
-        title: 'Index',
+        title: 'Site Design Index',
         stylesheet: 'indices',
         page: 'design'
     });
 });
+
+
+
+router.get('/green', function (req, res, next) {
+    res.render('design/green/index', {
+        title: 'green',
+        stylesheet: 'design/green',
+        page:'index',
+        site:'green'
+    });
+});
+router.get('/green/index', function (req, res, next) {
+    res.render('design/green/index', {
+        title: 'green',
+        stylesheet: 'design/green',
+        page:'index',
+        site:'green'
+    });
+});
+router.get('/green/about', function (req, res, next) {
+    res.render('design/green/about', {
+        title: 'green-about',
+        stylesheet: 'design/green',
+        page:'about',
+        site:'green'
+    });
+});
+router.get('/green/content', function (req, res, next) {
+    res.render('design/green/content', {
+        title: 'green-content',
+        stylesheet: 'design/green',
+        page:'content',
+        site:'green'
+    });
+});
+
+
 
 module.exports = router;
