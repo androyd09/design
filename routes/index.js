5575bd37287fd632d9cc9db8896fcc60ce767145
androyd09 <stevelight16@gmail.com>
2017-03-06 18:04:31 -0700

    about and other things
    Added about page
    added route and ejs
    added base style for articles on home page
    added dummy info
    added contact info in the footer
    added media query for header and main

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,16 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
-        title: 'Steven Light',
+        title: 'SAL Design',
         page: 'home'
+    });
+});
+
+/* GET about page. */
+router.get('/about', function (req, res, next) {
+    res.render('about', {
+        title: 'SAL Design',
+        page: 'about'
     });
 });
 
